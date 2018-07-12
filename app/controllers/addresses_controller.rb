0717ce905b926791dcf812a5453c6e917b94f836
wisetara <taratirzah@gmail.com>
2018-07-11 22:37:01 -0700

    Update addresses controller with validator service use

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,20 +1,29 @@
+require 'address_validator_service'
+
 class AddressesController < ApplicationController
   def index
     render 'new'
   end
 
-  def new; end
+  def new;
+  end
 
   def create
     @address = Address.new(address_params)
-    @address.save
-
-    render 'new'
+    if @address.save
+      flash.now[:notice] = 'You totes saved that address! Enter another?'
+      render 'new'
+    else
+      flash.now[:error] = 'Oh noes! That address may not be valid. Give it another go.'
+      render 'new', :status => 422
+    end
   end
 
   private
 
   def address_params
-    params.permit(:city, :state)
+    permitted_params = params.permit(:street_address, :city, :state, :zip_code)
+    permitted_params = permitted_params.values.join(' ')
+    AddressValidatorService.new(permitted_params).geocode_parse
   end
 end
