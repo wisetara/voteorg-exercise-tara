579434d82d08a201a44c6e87f99817e53bd77901
wisetara <taratirzah@gmail.com>
2018-07-11 22:37:38 -0700

    Update UI for address validator app

--- app/assets/javascripts/addresses.js
+++ app/assets/javascripts/addresses.js
@@ -1,2 +1,4 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).on "ajax:error", "form", (evt, xhr, status, error) ->
+   errors = xhr.responseJSON.error
+   for message of errors
+      $('#errors ul').append '<li>' + errors[message] + '</li>'
